e4adc12e7367a0fa0b3a18292056d02131bc4c20
Codexzier <johannes.paul.langner@live.de>
2021-11-28 15:47:35 +0100

    implement logging manager and logging view

---
+++ VoltageMeasurementLogger.Components/Log/LogItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VoltageMeasurementLogger.Components.Log
+{
+    public class LogItem
+    {
+        public LogItem(string content, DateTime date)
+        {
+            this.Content = content;
+            this.Written = date;
+
+            if(double.TryParse(this.Content, out var d))
+            {
+                this.NumericContent = d;
+            }
+        }
+
+        public string Content { get; }
+        public double NumericContent { get; }
+
+        public DateTime Written { get; }
+
+
+    }
+}
---
+++ VoltageMeasurementLogger.Components/Log/LogManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoltageMeasurementLogger.Components.Log
+{
+    public class LogManager
+    {
+        private static LogManager _instance;
+
+        private LogManager()
+        {
+        }
+
+        public static LogManager GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new LogManager();
+            }
+
+            return _instance;
+        }
+
+        public void WriteToFile(string filename)
+        {
+            string fullname = $"{PathOfLogFiles}{filename}";
+            if (File.Exists(fullname))
+            {
+                File.WriteAllText(fullname, string.Empty);
+                return;
+            }
+
+            File.Create(fullname);
+        }
+
+        public static string PathOfLogFiles => $"{Directory.GetCurrentDirectory()}/Logs/";
+    }
+}
